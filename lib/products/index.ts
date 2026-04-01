63155fe9a03aca2b2cd1a54f71f661fe0d42df37
nickshum88 <nick@marketplaceops.com>
2026-04-01 09:51:12 -0700

    Remove product: 14-in-1 Women's Inositol Complex Supplement

--- lib/products/index.ts
+++ lib/products/index.ts
@@ -13,7 +13,6 @@ import { extraStrengthTurmericBioperineGarlicGingerCapsules } from "./extra-stre
 import { melatoninFreeSleepSupportCapsules } from "./melatonin-free-sleep-support-capsules";
 import { melatoninSleepExtraStrengthCapsules10mg } from "./melatonin-sleep-extra-strength-capsules-10mg";
 import { milkThistleLiverSupportSupplementNacArtichokeDandelion } from "./milk-thistle-liver-support-supplement-nac-artichoke-dandelion";
-import { womensInositolComplexSupplement } from "./womens-inositol-complex-supplement";
 
 const products: Record<string, Product> = {
   "vitamin-b12-b6-liquid-drops": vitaminB12B6LiquidDrops,
@@ -30,7 +29,6 @@ const products: Record<string, Product> = {
   "melatonin-free-sleep-support-capsules": melatoninFreeSleepSupportCapsules,
   "melatonin-sleep-extra-strength-capsules-10mg": melatoninSleepExtraStrengthCapsules10mg,
   "milk-thistle-liver-support-supplement-nac-artichoke-dandelion": milkThistleLiverSupportSupplementNacArtichokeDandelion,
-  "womens-inositol-complex-supplement": womensInositolComplexSupplement,
 };
 
 export function getProduct(slug: string): Product | undefined {
